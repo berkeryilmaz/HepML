714a116341faa95ac3f8a8eaa2f846dc4e6d59f6
Berker Yilmaz <yilmaz.berker@gmail.com>
2023-11-04 15:48:20 +0300

    osiloskop klassı dictinorylerini küçük harf yapıldı kson kaydetme eklendi

--- Oscilloscope/OscilloscopeSetup.py
+++ Oscilloscope/OscilloscopeSetup.py
@@ -5,11 +5,12 @@ from Oscilloscope.Trig import Trig
 
 class OscilloscopeSetup:
     def __init__(self, setup_dict):
-        self.timebase = TimeBase(setup_dict['TIMEBASE'])
-        self.sample = Sample(setup_dict['SAMPLE'])
-        self.channel = [Channel(ch) for ch in setup_dict['CHANNEL']]
-        self.datatype = setup_dict['DATATYPE']
-        self.runstatus = setup_dict['RUNSTATUS']
-        self.idn = setup_dict['IDN']
-        self.model = setup_dict['MODEL']
-        self.trig = Trig(setup_dict['Trig'])
+        setup_dict = {key.lower(): setup_dict[key] for key in setup_dict}
+        self.timebase = TimeBase(setup_dict['timebase'])
+        self.sample = Sample(setup_dict['sample'])
+        self.channel = [Channel(ch) for ch in setup_dict['channel']]
+        self.datatype = setup_dict['datatype']
+        self.runstatus = setup_dict['runstatus']
+        self.idn = setup_dict['idn']
+        self.model = setup_dict['model']
+        self.trig = Trig(setup_dict['trig'])
