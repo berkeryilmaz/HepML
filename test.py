3b568dfb1acb954a5f262e3b553628cf0afc6823
Berker Yilmaz <yilmaz.berker@gmail.com>
2023-11-06 02:37:26 +0300

    klasör yapısı düzenlendi

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import os
+import json
 from Oscilloscope.FileReader import FileReader
 from Oscilloscope.Oscilloscope import Oscilloscope
 
 filelist = os.listdir()
 
-osiloskop = Oscilloscope(['0.bin', '1.bin'])
+osiloskop1 = Oscilloscope(['0.bin'])
+osiloskop2 = Oscilloscope(['1.bin'])
+
+if(osiloskop1.channel[1].data == osiloskop2.channel[1].data):
+    print('true')
+
 osiloskopfromjson = Oscilloscope.loadFromJson('berker/data.json')
 info = {'key' : 1234,'key2':456}
 for name, value in info.items():
-    print(name, value)
+    pass
+    #print(name, value)
 
 
 
