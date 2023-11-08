fb0137567a83edd0dc35916f771c5de4b434da6a
berkeryilmaz <yilmaz.berker@gmail.com>
2023-11-08 17:09:41 +0300

    plot fixed

--- folders.py
+++ folders.py
@@ -12,16 +12,17 @@ walk = os.walk(path)
 i = 1
 for (root, dirs, file) in os.walk(path):
     splitRoot = root.lower().split('/')
+    image_root = root.replace(path, 'berker')
     if (len(splitRoot) == 6 and len([name for name in file if name.endswith('.bin')]) > 0):
         osci = Oscilloscope([root + '/' + file_name for file_name in getOrderedFileList(root)])
         active_channel = osci.getActiveChannel()
         data_length = len(active_channel[0].data)
         for time_slice in time_slice_list:
-            params = createMeasureParams(splitRoot)
-            params['Time_Window'] = time_slice
             duration = time_slice * 5000
             begin, end = 0, duration
             while (end < data_length):
+                params = createMeasureParams(splitRoot)
+                params['Time_Window'] = time_slice
                 params['Peak_Count'] = active_channel[0].countPeaks(begin, end)
                 liste.append(params)
                 begin, end = end, end + duration
