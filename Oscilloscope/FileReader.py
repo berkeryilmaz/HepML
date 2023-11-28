d129c9bf7862fa992d33c1d270ce3957dd7cedf0
Berker Yilmaz <yilmaz.berker@gmail.com>
2023-11-29 00:09:41 +0300

    split_data_length control added

--- Oscilloscope/FileReader.py
+++ Oscilloscope/FileReader.py
@@ -2,6 +2,7 @@ import numpy as np
 import json
 from Oscilloscope.OscilloscopeSetup import OscilloscopeSetup
 
+
 class FileReader:
     def __init__(self, filepath=None):
         self.filePath = filepath
@@ -15,10 +16,15 @@ class FileReader:
         oscilloscope_setup_dict = json.loads(oscilloscope_setup_str)
         oscilloscope_setup = OscilloscopeSetup(oscilloscope_setup_dict)
 
+        split_data_length = len(split_data)
         for i, channel in enumerate(oscilloscope_setup.channel):
-            byte_data = np.frombuffer(split_data[1 + i], dtype=np.int16)
-            channel.raw_data = list(byte_data)
-            channel.setData(byte_data)
+            if (split_data_length > 1 + i):
+                byte_data = np.frombuffer(split_data[1 + i], dtype=np.int16)
+                channel.raw_data = list(byte_data)
+                channel.setData(byte_data)
+            else:
+                channel.raw_data = [0] * oscilloscope_setup.sample.datalen
+                channel.setData(channel.raw_data)
         return oscilloscope_setup
 
     def readFromJson(self):
